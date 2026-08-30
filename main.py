211a8bba96f0bca298e1098c9be216b046f21d7a
abol <abolabol@gmail.com>
2026-08-30 10:31:27 +0330

    abol/adding : adding function add_tesk in project

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ print("1 . add task")
 print("2 . show task")
 print("3 . Delet task")
 print("4 . Exit task")
-choose_task_option = input("you choose your option :")
+task_List = []
+def add_task(task_List):
+    name_task = input("what is name task :")
+    if(len(name_task) <= 0 ):
+        print("you have to writing 1 word")   
+    else :
+        task_List.append(name_task)
+        print("task added")
 
-
+    
 while True : 
+    choose_task_option = input("you choose your option :")
     if(choose_task_option == "1"):
-        add_task()
+        add_task(task_List)
  
