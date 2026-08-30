e1d459d3bd5ddd71596a5b56c5da2a8649de66fc
abol <abolabol@gmail.com>
2026-08-30 09:55:55 +0330

    started new project

---
+++ main.py
@@ -0,0 +1,12 @@
+# lst's start new project 
+print("1 . add task")
+print("2 . show task")
+print("3 . Delet task")
+print("4 . Exit task")
+choose_task_option = input("you choose your option :")
+
+
+while True : 
+    if(choose_task_option == "1"):
+        add_task()
+ 
