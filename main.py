3f430121b85c38386b124927f08663d6f2dd5a52
abol <abolabol@gmail.com>
2026-08-31 10:20:59 +0330

    abol/updateAndAddingDelete : before code upadated and delet code added

--- main.py
+++ main.py
@@ -9,20 +9,36 @@ def add_task(task_List):
     if(len(name_task) <= 0 ):
         print("you have to writing 1 word")   
     else :
-        task_List[name_task] = task_List
-        print("task added")
-    return task_List    
+        if task_List.get(name_task):
+            print("not key rpate")
+        else :    
+            task_List[name_task] = task_List
+            print("task added")    
 def showTesk(addTeskList):
     for i in addTeskList:
         print(i)
+def deleting(items):
+    deleted = input("what is deleted tesk")
+    if(len(deleted) <= 0 ):
+        print("you have to writing 1 word")  
+    else : 
+        if items.get(deleted) : 
+            items.pop(deleted)
+            print(f"item {deleted} deleted")
+        else : print("not find")        
+    
+            
             
 
-returnedAddtesk =[]
+
 while True : 
     choose_task_option = input("you choose your option :")
     if(choose_task_option == "1"):
-       returnedAddtesk =  add_task(task_List)
+       add_task(task_List)
     if(choose_task_option == "2"):
-        showTesk(returnedAddtesk)
+        showTesk(task_List)
+    if(choose_task_option == "3"):
+        deleting(task_List)
+        
         
  
