349ffc43c62d77b30a6a02aebcb72ac1c12bf080
abol <abolabol@gmail.com>
2026-08-31 10:30:31 +0330

    abol/updatingVorody : vorody updated and refactoring

--- main.py
+++ main.py
@@ -4,21 +4,23 @@ print("2 . show task")
 print("3 . Delet task")
 print("4 . Exit task")
 task_List = {}
+def vorody(testVoridy):
+   return input(testVoridy)
 def add_task(task_List):
-    name_task = input("what is name task :")
-    if(len(name_task) <= 0 ):
+    addNameTesk = vorody("what is name task :")
+    if(len(addNameTesk) <= 0 ):
         print("you have to writing 1 word")   
     else :
-        if task_List.get(name_task):
+        if task_List.get(addNameTesk):
             print("not key rpate")
         else :    
-            task_List[name_task] = task_List
+            task_List[addNameTesk] = task_List
             print("task added")    
 def showTesk(addTeskList):
     for i in addTeskList:
         print(i)
 def deleting(items):
-    deleted = input("what is deleted tesk")
+    deleted = vorody("which delete you tesk : ")
     if(len(deleted) <= 0 ):
         print("you have to writing 1 word")  
     else : 
