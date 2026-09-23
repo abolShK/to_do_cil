89fab3a0d0a88795c6d25308b67b67a8378e4b88
Abol <laptop_safir@DESKTOP-0D13BB7.localdomain>
2026-09-23 11:39:07 +0330

    addingTest/abol : adding test add Archive

---
+++ tests/test_Aechive.py
@@ -0,0 +1,20 @@
+import unittest
+from Archive import add_archive
+class TEST(unittest.TestCase):
+    def test_add_archive(self):
+        archiveList = {
+            
+        }
+        taskList={
+            "ali": {"status": "true", "priority": "Low", "pin": "false", "date": "2026-09-23", "RecurringTask": "none"},
+            "ts": {"status": "true", "priority": "Low", "pin": "false", "date": "2026-09-15" , "RecurringTask" : "none"}
+        }
+        def fake(data) :
+            pass
+        add_archive(taskList , archiveList , [] , fake , "ts")
+        newtaskList = {"ali": {"status": "true", "priority": "Low", "pin": "false", "date": "2026-09-23", "RecurringTask": "none"}}
+        newarchiveList = {"ts": {"status": "true", "priority": "Low", "pin": "false", "date": "2026-09-15" , "RecurringTask" : "none"}}
+        self.assertIn("ts", newarchiveList)
+        self.assertNotIn("ts", newtaskList)
+            
+        
